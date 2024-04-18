173ccb102e26e5dcbcadd0a13ad601be0858a127
Oleh Andrusenko <dev.andrusenko.oleh@gmail.com>
2024-04-18 13:34:16 +0300

    styling changes

--- components/Home.jsx
+++ components/Home.jsx
@@ -9,27 +9,28 @@ function Home() {
   const router = useRouter()
   if (session.status === "loading") return <div className='loader'></div>
   if (session.status === "authenticated") return router.push("/dashboard")
-  return (
-    <div className='w-[600px] h-[500px] flex flex-col justify-center items-center'>
-      <div className='text-2xl font-semibold my-8'>
-        Welcome to our <span className='text-blue-700'>app</span>! Sign in to
-        continue!
+  if (session.status === "unauthenticated")
+    return (
+      <div className='w-[600px] h-[500px] flex flex-col justify-center items-center'>
+        <div className='text-2xl font-semibold my-8'>
+          Welcome to our <span className='text-blue-700'>app</span>! Sign in to
+          continue!
+        </div>
+        <Image
+          src={signin}
+          alt=''
+          width={250}
+          height={250}
+          className='mx-auto my-10'
+        />
+        <Link
+          href='/login'
+          className='w-48 h-10 bg-blue-700 flex justify-center items-center rounded-lg text-white hover:scale-105 transition-all'
+        >
+          Sign in
+        </Link>
       </div>
-      <Image
-        src={signin}
-        alt=''
-        width={250}
-        height={250}
-        className='mx-auto my-10'
-      />
-      <Link
-        href='/login'
-        className='w-48 h-10 bg-blue-700 flex justify-center items-center rounded-lg text-white hover:scale-105 transition-all'
-      >
-        Sign in
-      </Link>
-    </div>
-  )
+    )
 }
 
 export default Home
